c39cc1cf63fbe9ddda90f55f6b35513642e45adb
haikalirhamna <haikalirhamna15@gmail.com>
2023-11-26 20:18:40 +0700

    init

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,13 +9,41 @@ module.exports = {
 		},
 		extend: {
 			colors: {
-				grey: '#969696',
-				primary: '#FF994F',
-				secondary: '#FFFAF2',
-				dark: '#151F3C',
-				chocolate: '#281900',
-				'white-green': '#B4FF86',
+				primary: '#03AC0E',
+				second: '#FF7F17',
 			},
+			fontSize: {
+				'7xl': ['80px', {
+					lineHeight: '96px',
+					letterSpacing: '-0.01em',
+					fontWeight: '500',
+				}],
+				'6xl': ['64px', {
+					lineHeight: '83.2px',
+					letterSpacing: '-0.01em',
+					fontWeight: '700',
+				}],
+				'5xl': ['40px', {
+					lineHeight: '45px',
+					letterSpacing: '-0.01em',
+					fontWeight: '700',
+				}],
+				'2xl': ['28px', {
+					lineHeight: '30px',
+					letterSpacing: '-0.02em',
+					fontWeight: '400',
+				}],
+				'xl': ['20px', {
+					lineHeight: '30px',
+					letterSpacing: '-0.02em',
+					fontWeight: '400',
+				}],
+				'sm' : ['14px', {
+					lineHeight: '22px',
+					letterSpacing: '-0.02em',
+					fontWeight: '400',
+				}],
+			}
 		},
 	},
 	variants: {
