7a2f4d807461a2966b968f59812bbb57c25b9caa
haikalirhamna <haikalirhamna15@gmail.com>
2023-11-27 09:28:41 +0700

    change

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,8 @@
 import '../sass/style.scss';
+
+const btnHamburger = document.querySelector('#btn-hamburger')
+const hamburgerMenu = document.querySelector('.humburger-menu')
+
+btnHamburger.addEventListener(('click'), () => {
+  hamburgerMenu.classList.toggle('show')
+})
--- src/js/toggleButton.js
+++
@@ -1,6 +0,0 @@
-btnHamburger = document.querySelector('#btn-hamburger')
-hamburgerMenu = document.querySelector('.humburger-menu')
-
-btnHamburger.addEventListener(('click'), () => {
-  hamburgerMenu.classList.toggle('show')
-})
